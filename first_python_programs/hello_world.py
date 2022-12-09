1c9f8e2828143f14ae8bd2a4abcbf642ad732c25
Ashley Singh <34393084+ashsi@users.noreply.github.com>
2022-12-09 15:28:58 +0000

    Create hello_world.py

---
+++ first_python_programs/hello_world.py
@@ -0,0 +1,10 @@
+# Print user's name and age.
+
+name = input("Please enter your name: ")
+print(name)
+
+age = input("Please enter your age: ")
+print(age)
+
+# Print Hello World!
+print("Hello World!")
