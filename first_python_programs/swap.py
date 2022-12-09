42d99039e84c197d07cb2f5bcb54ccef44d5c7bc
Ashley Singh <34393084+ashsi@users.noreply.github.com>
2022-12-09 15:34:40 +0000

    Create swap.py

---
+++ first_python_programs/swap.py
@@ -0,0 +1,29 @@
+# Swapping two numbers from the user.
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+print("\nBefore the swap.")
+print(f"num1: {num1}, num2: {num2}\n")
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After the swap.")
+print(f"num1: {num1}, num2: {num2}")
+
+
+# Alternative solution without declaring a temporary variable. 
+# Change is in line 26, replacing lines 9-11.
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+print("\nBefore the swap.")
+print(f"num1: {num1}, num2: {num2}\n")
+
+num1, num2 = num2, num1
+
+print("After the swap.")
+print(f"num1: {num1}, num2: {num2}")
