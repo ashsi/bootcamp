ab0155e685a669c773066c8f7bd54fcd410448bc
Ashley Singh <34393084+ashsi@users.noreply.github.com>
2022-12-09 15:31:57 +0000

    Create user_details.py

---
+++ first_python_programs/user_details.py
@@ -0,0 +1,8 @@
+# Print user's personal details.
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+house_no = input("Enter your house number: ")
+st_name = input("Enter your street name: ")
+
+print(f"This is {name} who is {age} years old and lives at {house_no} {st_name}.")
